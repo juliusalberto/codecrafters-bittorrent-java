4bb2d714893ddea074349f5b72d42dd69eeccd3a
juliusalberto <u7724204@anu.edu.au>
2024-10-07 11:18:26 +1100

    Enable decoding bencoded integer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,44 +2,59 @@ import com.google.gson.Gson;
 // import com.dampcake.bencode.Bencode; - available if you need it!
 
 public class Main {
-  private static final Gson gson = new Gson();
+    private static final Gson gson = new Gson();
 
-  public static void main(String[] args) throws Exception {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    public static void main(String[] args) throws Exception {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
 //    System.out.println("Logs from your program will appear here!");
-    String command = args[0];
-    if("decode".equals(command)) {
-      //  Uncomment this block to pass the first stage
-        String bencodedValue = args[1];
-        String decoded;
-        try {
-          decoded = decodeBencode(bencodedValue);
-        } catch(RuntimeException e) {
-          System.out.println(e.getMessage());
-          return;
-        }
-        System.out.println(gson.toJson(decoded));
+        String command = args[0];
+        if("decode".equals(command)) {
+            //  Uncomment this block to pass the first stage
+            String bencodedValue = args[1];
+            Object decoded;
+            try {
+                decoded = decodeBencode(bencodedValue);
+            } catch(RuntimeException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+            System.out.println(gson.toJson(decoded));
 
-    } else {
-      System.out.println("Unknown command: " + command);
+        } else {
+            System.out.println("Unknown command: " + command);
+        }
+
     }
 
-  }
+    static Object decodeBencode(String bencodedString) {
+        if (Character.isDigit(bencodedString.charAt(0))) {
+            int firstColonIndex = 0;
+            for(int i = 0; i < bencodedString.length(); i++) {
+                if(bencodedString.charAt(i) == ':') {
+                    firstColonIndex = i;
+                    break;
+                }
+            }
+            int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
+            return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
+        } else if (bencodedString.charAt(0) == 'i') {
+            int eIndex = 0;
 
-  static String decodeBencode(String bencodedString) {
-    if (Character.isDigit(bencodedString.charAt(0))) {
-      int firstColonIndex = 0;
-      for(int i = 0; i < bencodedString.length(); i++) { 
-        if(bencodedString.charAt(i) == ':') {
-          firstColonIndex = i;
-          break;
+            for (int i = 1; i < bencodedString.length(); i++) {
+                if (bencodedString.charAt(i) == 'e') {
+                    eIndex = i;
+                    break;
+                }
+            }
+
+            // Now we already have the index of e
+            // We want to get the substring from 1 - e
+            String numString = bencodedString.substring(1, eIndex);
+            return Integer.parseInt(numString);
         }
-      }
-      int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-      return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
-    } else {
-      throw new RuntimeException("Only strings are supported at the moment");
+        else {
+            throw new RuntimeException("Only strings are supported at the moment");
+        }
     }
-  }
-  
+
 }
